219863669107029ffec83e8eadbcb1d96f850faa
michele malagnini <michelemalagn@gmail.com>
2021-04-07 16:07:23 +0200

    fix sum

--- range-sum.js
+++ range-sum.js
@@ -1,42 +1,46 @@
+
+
 function range(start,end){
     let arr = [];
     for(let i = start; i <= end; i++){
-    arr.push(i);
+        arr.push(i);
     }
     return arr
-  }
-  
-  function rangeIntervall(start,end,intervall){
-    if (intervall == null){
-      intervall = 1;
-    }
+}
+
+function rangeStep(start,end, step){
     let arr = [];
-    for(let i = start; i <= end; i +=  intervall){
-      arr.push(i);
+    if(step == null){
+        step = 1;
     }
+    if(step > 0){
+        for(let i = start; i <= end; i +=  step){
+            arr.push(i);
+        }   
+    }
+    if(step < 0){
+        for(let i = start; i >= end; i +=  step){
+            arr.push(i);
+        }   
+    }
+    
     return arr
-  }
-  
-  let arrNormale = range(1,10);
-  console.log('array: ' + arrNormale);
-  
-  let arrIntervall = rangeIntervall(1,10,2);
-  console.log('array intervall: ' + arrIntervall);
-  
-  function sum(arr){
+}
+
+
+
+
+function sum(arr){
     let totale = 0;
     for(let i = 0; i < arr.length; i++){
-        totale +=  arr[i];
+        totale = totale + arr[i]
     }
     return totale
-  }
+}
 
-// function sum(array) {
-//     var total = 0;
-//     for (var i = 0; i < array.length; i++)
-//       total += array[i];
-//     return total;
-// }
-  
-  console.log('somma degli elementi dell array: ' + sum(arrNormale))
-  console.log('somma degli elementi dell arrayIntervall: ' + sum(arrIntervall))
+
+console.log(range(1,10));
+console.log(rangeStep(5,2,-1));
+
+console.log(sum(range(1,10)));
+console.log(sum(rangeStep(5,2,-1)));
